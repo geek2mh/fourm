76d2c9d60fdaf006af4cea84980c36e6f0a0989e
geek2mh <geek2mh@gmail.com>
2017-03-15 04:57:45 +0200

    Add files via upload

---
+++ lang/Dutch.js
@@ -0,0 +1,227 @@
+// Dutch (NL) Translation
+// Translation by Samantha and Joost (http://fmdesign.forumotion.com/t738-forumactif-edge-dutch-translation#14469)
+FAE.lang = {
+
+  language : 'Dutch',
+
+  css : {
+    offline : 'OFFLINE',
+    online : 'ONLINE',
+    total_posts : 'Totaal aantal berichten',
+    total_topics : 'Totaal aantal onderwerpen',
+    total_users : 'Totaal aantal leden',
+    record_users : 'Meeste leden ooit online',
+    board_age : 'Leeftijd van het forum (in dagen)',
+    newest_member : 'Nieuwste lid',
+    hidebox : 'Verborgen:',
+    chatbox_tip : 'Beweeg hier met de muis over om te zien wie er online is.',
+    chatbox_no_away : 'Er zijn geen leden inactief.',
+    chatbox_no_online : 'Er zijn geen leden online.'
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'https://i37.servimg.com/u/f37/18/45/41/65/new-to10.png?color=primary&size=m',
+      i_reply : 'http://i35.servimg.com/u/f35/18/21/60/73/post-r11.png?color=primary&size=m',
+      i_reply_locked : 'http://i35.servimg.com/u/f35/18/21/60/73/locked11.png?color=secondary&size=m',
+      i_icon_quote : 'http://i35.servimg.com/u/f35/18/21/60/73/quote-11.png?color=primary&size=s',
+      i_icon_multiquote_off : 'http://i35.servimg.com/u/f35/18/21/60/73/multi-11.png?color=primary&size=s',
+      i_icon_multiquote_on : 'http://i35.servimg.com/u/f35/18/21/60/73/multi-11.png?color=secondary&size=s',
+      i_icon_thanks_off : 'http://i35.servimg.com/u/f35/18/21/60/73/thanks11.png?color=primary&size=s',
+      i_icon_thanks_on : 'http://i35.servimg.com/u/f35/18/21/60/73/thanks11.png?color=secondary&size=s',
+      i_icon_edit : 'http://i35.servimg.com/u/f35/18/21/60/73/edit-n10.png?color=primary&size=s',
+      i_icon_delete : 'http://i86.servimg.com/u/f86/18/21/41/30/delete10.png?color=primary&size=s',
+      i_icon_ip : 'http://i86.servimg.com/u/f86/18/21/41/30/ip-f10.png?color=primary&size=s',
+      i_icon_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=primary&size=s',
+      i_icon_report_new : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=secondary&size=s',
+      i_icon_report_locked : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=secondary&size=s',
+      i_icon_lock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=primary&size=s',
+      i_icon_unlock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report13.png?color=primary&size=s',
+      i_icon_search : FAE.eGIF,
+      i_icon_profile : 'http://i86.servimg.com/u/f86/18/21/41/30/profil12.png?color=primary&size=s',
+      i_icon_www : 'http://i86.servimg.com/u/f86/18/21/41/30/websit10.png?color=primary&size=s',
+      i_icon_email : 'http://i86.servimg.com/u/f86/18/21/41/30/email-10.png?color=primary&size=s',
+      i_icon_pm : 'http://i86.servimg.com/u/f86/18/21/41/30/pm-f11.png?color=primary&size=s',
+      i_icon_fb : 'http://i86.servimg.com/u/f86/18/21/41/30/facebo11.png?color=primary&size=s',
+      i_icon_twitter : 'http://i86.servimg.com/u/f86/18/21/41/30/twitte10.png?color=primary&size=s',
+      i_icon_pinterest : 'http://i86.servimg.com/u/f86/18/21/41/30/pinter10.png?color=primary&size=s',
+      i_icon_aim : 'http://i86.servimg.com/u/f86/18/21/41/30/aim-f10.png?color=primary&size=s',
+      i_icon_icq_add : 'http://i86.servimg.com/u/f86/18/21/41/30/icq-f10.png?color=primary&size=s',
+      i_icon_msnm : 'http://i86.servimg.com/u/f86/18/21/41/30/msn-f10.png?color=primary&size=s',
+      i_icon_yim : 'http://i86.servimg.com/u/f86/18/21/41/30/yahoo-10.png?color=primary&size=s',
+      i_icon_skype : 'http://i86.servimg.com/u/f86/18/21/41/30/skype-10.png?color=primary&size=s',
+      i_icon_online : FAE.eGIF,
+      i_msg_newpost : 'http://i35.servimg.com/u/f35/18/21/60/73/new-pm11.png?color=primary&size=m',
+      i_msg_inbox : FAE.eGIF,
+      i_msg_sentbox : FAE.eGIF,
+      i_msg_outbox : FAE.eGIF,
+      i_msg_savebox : FAE.eGIF,
+      i_icon_ajax_edit : 'http://i86.servimg.com/u/f86/18/21/41/30/edit-f10.png',
+      i_icon_ajax_valid : 'http://i86.servimg.com/u/f86/18/21/41/30/valid-10.png',
+      i_icon_attachment_see : 'http://i86.servimg.com/u/f86/18/21/41/30/show-a10.png',
+      i_icon_attachment_download : 'http://i86.servimg.com/u/f86/18/21/41/30/downlo10.png',
+
+      // DO NOT TRANSLATE
+      page : 'buttons',
+      submit : 'Save'
+    },
+
+    gallery : {
+      upload_pic : 'https://i37.servimg.com/u/f37/18/45/41/65/upload10.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : '<div class="title module_column_title">Widget Menu</div>',
+      widget_menu_tooltip : "menu.title = 'Widget menu laten zien of verbergen';",
+
+      select_theme : 'Kies een thema ...',
+      random_theme : 'Willekeurig thema',
+      default_theme : 'Standaard thema',
+
+      toolbar_search_placeholder : 'Zoeken ...',
+
+      quick_nav_title : "title : 'Snelle Navigatie'",
+      quick_nav_tooltip : "tooltip : 'Snelle Navigatie (de-)activeren'",
+      quick_nav_notif : 'Notificatie types aanpassen',
+      tool_copy_url_tooltip : 'Kopieer BBCode URL',
+      tool_copy_url_prompt : 'Kopieer BBCode URL hieronder (CTRL+C)',
+      tool_admin_panel : 'Beheerderspaneel',
+      tool_to_top : 'Top van de pagina',
+      tool_to_bottom : 'Bodem van de pagina',
+
+      img_resizer_full_size : 'Ware grootte',
+      img_resizer_enlarge : 'Vergroot plaatje',
+      img_resizer_reduce : 'Verklein plaatje',
+      img_resizer_download : 'Download plaatje',
+      img_resizer_tooltip : 'Klik om het volledige plaatje te zien',
+
+      fae_update_found : 'Er is een update voor Forumactif Edge beschikbaar. Klik op "Updates Zoeken" vanuit het FAE Beheerderspaneel om het installatieproces te beginnen.'
+    },
+
+
+    '[FA EDGE] TOPICS.JS' : {
+      thanked_message : '<div class="thanked-message"><i class="fa fa-thumbs-up"></i> {USERNAME} werd bedankt door diegene die het onderwerp had gestart !</div>'
+    }
+
+  },
+
+
+  templates : {
+    'overall_header.html' : {
+      announcements : '<div id="fa_ticker_title" class="title">Mededelingen</div>'
+    },
+
+
+    'overall_footer_end.html' : {
+      back_to_top : 'Terug naar boven',
+      fae_feedback : 'Feedback versturen',
+      fae_feedback_tooltip : 'Help Forumactif Edge te verbeteren'
+    },
+
+
+    logged_out_reply : {
+      main_title : '<h2 class="lor_maintitle">Registreer of log in om te kunnen reageren</h2>',
+      main_desc : '<p class="lor_maindesc">U dient een geregistreerd lid te zijn om te kunnen reageren.</p>',
+
+      create_account_title : '<h3 class="lor_subtitle">Registreren</h3>',
+      create_account_desc : '<p class="lor_subdesc">Word nu lid van onze community door hier te registreren. Het is makkelijk!</p>',
+      create_account_button : '<a href="/register" class="button1">Registeren</a>',
+
+      log_in_title : '<h3 class="lor_subtitle">Inloggen</h3>',
+      log_in_desc : '<p class="lor_subdesc">Bent u al lid? Welkom terug! U kunt hier inloggen.</p>',
+      log_in_button : '<a href="/login" class="button1">Inloggen</a>'
+    }
+
+  },
+
+
+  fae_cp : {
+    fae_cp_main_title : 'Forumactif Edge Beheerderspaneel',
+    fae_cp_desc : 'Via dit paneel kunt u <a href="https://github.com/SethClydesdale/forumactif-edge" target="_blank">Forumactif Edge</a> configureren, installeren, installaties ongedaan maken en updaten. Ervaar het forum op een modernere en sympathiekere wijze dan ooit tevoren!',
+    fae_log : 'Logbestand',
+    fae_actions : 'Acties',
+
+    fae_install : 'Installeren',
+    fae_reinstall : 'Herinstalleren',
+    fae_install_complete : 'KLAAR!',
+    fae_install_warning : 'Weet u zeker dat u Forumactif Edge wilt {INSTALL} ? Het huidige thema, inclusief uw CSS, zal worden overschreven en al uw JavaScripts zullen worden verwijderd.\\\n\\\nWij adviseren u om een backup te maken van al uw persoonlijk aangepaste content bestanden, zoals CSS, de templates en al uw JavaScripts, alvorens door te gaan met de installatie. Klik op "Cancel" als u nu nog niet door wilt gaan met de installatie van Forumactif Edge.',
+
+    fae_uninstall : 'Installatie ongedaan maken',
+    fae_uninstall_warning : 'Weet u zeker dat u de installatie van Forumactif Edge ongedaan wilt maken? Alle huidige CSS, JavaScripts en templates zullen worden verwijderd. Het forum zal worden teruggezet naar het standaard phpBB3 thema.\\\n\\\nWij adviseren u om een backup te maken van al uw persoonlijk aangepaste content bestanden, zoals CSS, de templates en al uw JavaScripts alvorens door te gaan met de installatie. Klik op "Cancel" als u nu nog niet door wilt gaan met de de-installatie van Forumactif Edge.',
+
+    fae_update : 'Updates zoeken',
+    fae_update_good : 'Gefeliciteerd! Forumactif Edge is helemaal up to date!',
+    fae_update_check : 'Bezig met zoeken naar updates op Github ...',
+    fae_update_prepare : 'Bezig met laden van de update instructies. Sluit dit tabblad a.u.b. NIET! ...',
+    fae_update_start : 'Bezig met starten van het update proces. Sluit dit tabblad a.u.b. NIET! ...',
+    fae_update_fetch : 'Bezig met laden van update instructies voor versie {VERSION_STRING}... ({UPDATE_MIN}/{UPDATE_MAX})',
+    fae_update_notes : '<a href="{RELEASE_TAG}" target="_blank">Klik hier</a> om de wijzigingen te bekijken van versie {VERSION_STRING}.',
+    fae_update_error : 'De update instructies voor versie {VERSION_STRING} konden niet worden gevonden. Maak alstublieft <a href="http://fmdesign.forumotion.com/t700-forumactif-edge-support#13923" target="_blank">hier</a> een support ticket aan (Engels) met alle benodigde informatie over dit probleem, zodat we u zo spoedig mogelijk verdere assistentie kunnen verlenen.',
+
+    fae_translate : 'Wijzig taal',
+    fae_translate_same : 'Forumactif Edge wordt reeds weergegeven in {LANG}. Kies een andere taal.',
+    fae_translate_warning : 'Weet u zeker dat u de taalweergave in Forumactif Edge wilt wijzigen naar {LANG} ?',
+    fae_translate_start : 'Bezig met voorbereiden van de taalwijziging. Even geduld a.u.b. ...',
+    fae_translate_get : 'Bezig met laden van de taal data voor {LANG} ...',
+    fae_translate_loaded : 'De nieuwe taal is geladen. De taalwijzigingen worden nu doorgevoerd. Sluit dit tabblad a.u.b. NIET! ...',
+
+    fae_err_not_founder : 'lechts <a href="/u1">de eigenaar</a> is gemachtigd het FAE Beheerderspaneel te gebruiken. Neem daarom contact op met de eigenaar van het forum voor verdere hulp bij de installatie van Forumactif Edge of (het wijzigen van) één van de componenten.',
+
+    left : 'Links',
+    center : 'Centreren',
+    right : 'Rechts',
+    yes : 'Ja',
+    no : 'Nee',
+    help : 'Help!',
+
+    general_settings : {
+      title : 'Algemene Instellingen',
+      fae_gen_desc : 'In deze sectie kunt u de algemene instellingen van Forumactif Edge beheren.',
+
+      'fae_label-width' : 'Forum breedte :',
+      'fae_gen_tip-width' : 'Pas de forum breedte aan door de schuif naar links of naar rechts te bewegen.',
+
+      'fae_label-navbar' : 'Navigatiebalk positie :',
+      'fae_gen_tip-navbar' : 'Positie van de navigatiebalk links.',
+
+      'fae_label-logo' : 'Logo positie :',
+      'fae_gen_tip-logo' : 'Positie van het logo van het forum.',
+
+      'fae_label-profile' : 'Profiel positie :',
+      'fae_gen_tip-profile' : 'Positie van het profiel in de onderwerpen en berichten.',
+
+      fae_update_general : 'Wijzigingen opslaan',
+      fae_gen_updating : 'Bezig met het opslaan van de wijzigingen in de Algemene Instellingen ...',
+      fae_gen_updated : 'De wijzigingen in de Algemene Instellingen zijn succesvol opgeslagen!'
+    },
+
+
+    theme_management : {
+      title : 'Thema Beheer',
+      fae_theme_desc : 'In deze sectie kunt u het standaard thema en de kleuren binnen Forumactif Edge aanpassen en beheren door nieuwe thema\'s uit de database op Github te importeren.',
+
+      'fae_label-theme' : 'Kies een thema :',
+      'fae_theme_tip-theme' : 'Kies het type thema dat u wilt importeren.',
+
+      'fae_label-color' : 'Kies een kleur :',
+      'fae_theme_tip-color' : 'Kies de standaard kleur voor het gekozen thema.',
+
+      'fae_label-minify' : 'CSS optimaliseren :',
+      'fae_theme_tip-minify' : 'Geoptimaliseerde CSS neemt minder plaats in en maakt uw forum sneller, maar het maakt het ook moeilijker om deze als beheerder te lezen of aan te passen. Indien u de CSS voor het gekozen thema in de toekomst wenst aan te passen en u bent niet bekend met CSS, kies dan "Nee".',
+
+      'fae_label-rtl' : 'Rechts-naar-Links schrijven :',
+      'fae_theme_tip-rtl' : 'Wijzigt binnen het thema welke kant op wordt geschreven (Links-naar-Rechts of Rechts-naar-Links). In sommige talen, zoals in het Arabisch, is dit anders.',
+
+      fae_import_theme : 'Importeer thema',
+      fae_import_confirm : 'Weet u zeker dat u het "{THEME_NAME}" thema wilt importeren ?\\\n\\\nTijdens de installatie van het nieuwe thema zal de volledige CSS worden vervangen. Weest u er alstublieft zeker van dat u een backup heeft van uw huidige CSS als u deze wilt bewaren. Kies "Cancel" als u er nu nog niet klaar voor bent het nieuwe thema te importeren.'
+    }
+  }
+
+};
