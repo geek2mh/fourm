a036463b8aeeb2afe909c9f3f0b172e52d87d801
geek2mh <geek2mh@gmail.com>
2017-03-15 04:53:53 +0200

    Add files via upload

---
+++ 1/Arabic.js
@@ -0,0 +1,227 @@
+// Arabic Translation
+// Translation by omarpop23, Michael_vx, Gin NeOs, amghidh (http://fmdesign.forumotion.com/t740-forumactif-edge-arabic-translation#14583)
+FAE.lang = {
+
+  language : 'العربية',
+
+  css : {
+    offline : 'غير متصل',
+    online : 'متصل',
+    total_posts : 'إجمالي المساهمات',
+    total_topics : 'إجمالي المواضيع',
+    total_users : 'إجمالي عدد الأعضاء',
+    record_users : 'أكبر عدد من المتصلين',
+    board_age : 'عمر المنتدى (بالأيام)',
+    newest_member : 'آخر عضو مسجّل',
+    hidebox : 'محتوى مخفي:',
+    chatbox_tip : 'ضع مؤشر الفأرة هنا لمعاينة المتصلين.',
+    chatbox_no_away : 'لا يوجد حاليا أعضاء غائبين...',
+    chatbox_no_online : 'لا يوجد حاليا عضو متصل...'
+  },
+
+
+  images : {
+
+    buttons : {
+      i_post : 'http://i35.servimg.com/u/f35/18/21/41/30/new_to11.png?color=primary&size=m',
+      i_reply : 'http://i35.servimg.com/u/f35/18/21/41/30/post_r11.png?color=primary&size=m',
+      i_reply_locked : 'http://i35.servimg.com/u/f35/18/21/41/30/locked12.png?color=secondary&size=m',
+      i_icon_quote : 'http://i35.servimg.com/u/f35/18/21/41/30/quote-12.png?color=primary&size=s',
+      i_icon_multiquote_off : 'http://i35.servimg.com/u/f35/18/21/41/30/multi-12.png?color=primary&size=s',
+      i_icon_multiquote_on : 'http://i35.servimg.com/u/f35/18/21/41/30/multi-12.png?color=secondary&size=s',
+      i_icon_thanks_off : 'http://i35.servimg.com/u/f35/18/21/41/30/thanks12.png?color=primary&size=s',
+      i_icon_thanks_on : 'http://i35.servimg.com/u/f35/18/21/41/30/thanks12.png?color=secondary&size=s',
+      i_icon_edit : 'http://i35.servimg.com/u/f35/18/21/41/30/edit-a11.png?color=primary&size=s',
+      i_icon_delete : 'http://i86.servimg.com/u/f86/18/21/41/30/delete10.png?color=primary&size=s',
+      i_icon_ip : 'http://i86.servimg.com/u/f86/18/21/41/30/ip-f10.png?color=primary&size=s',
+      i_icon_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=primary&size=s',
+      i_icon_report_new : 'http://i86.servimg.com/u/f86/18/21/41/30/report10.png?color=secondary&size=s',
+      i_icon_report_locked : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=secondary&size=s',
+      i_icon_lock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report12.png?color=primary&size=s',
+      i_icon_unlock_report : 'http://i86.servimg.com/u/f86/18/21/41/30/report13.png?color=primary&size=s',
+      i_icon_search : FAE.eGIF,
+      i_icon_profile : 'http://i86.servimg.com/u/f86/18/21/41/30/profil12.png?color=primary&size=s',
+      i_icon_www : 'http://i86.servimg.com/u/f86/18/21/41/30/websit10.png?color=primary&size=s',
+      i_icon_email : 'http://i86.servimg.com/u/f86/18/21/41/30/email-10.png?color=primary&size=s',
+      i_icon_pm : 'http://i86.servimg.com/u/f86/18/21/41/30/pm-f11.png?color=primary&size=s',
+      i_icon_fb : 'http://i86.servimg.com/u/f86/18/21/41/30/facebo11.png?color=primary&size=s',
+      i_icon_twitter : 'http://i86.servimg.com/u/f86/18/21/41/30/twitte10.png?color=primary&size=s',
+      i_icon_pinterest : 'http://i86.servimg.com/u/f86/18/21/41/30/pinter10.png?color=primary&size=s',
+      i_icon_aim : 'http://i86.servimg.com/u/f86/18/21/41/30/aim-f10.png?color=primary&size=s',
+      i_icon_icq_add : 'http://i86.servimg.com/u/f86/18/21/41/30/icq-f10.png?color=primary&size=s',
+      i_icon_msnm : 'http://i86.servimg.com/u/f86/18/21/41/30/msn-f10.png?color=primary&size=s',
+      i_icon_yim : 'http://i86.servimg.com/u/f86/18/21/41/30/yahoo-10.png?color=primary&size=s',
+      i_icon_skype : 'http://i86.servimg.com/u/f86/18/21/41/30/skype-10.png?color=primary&size=s',
+      i_icon_online : FAE.eGIF,
+      i_msg_newpost : 'http://i35.servimg.com/u/f35/18/21/41/30/new_pm11.png?color=primary&size=m',
+      i_msg_inbox : FAE.eGIF,
+      i_msg_sentbox : FAE.eGIF,
+      i_msg_outbox : FAE.eGIF,
+      i_msg_savebox : FAE.eGIF,
+      i_icon_ajax_edit : 'http://i86.servimg.com/u/f86/18/21/41/30/edit-f10.png',
+      i_icon_ajax_valid : 'http://i86.servimg.com/u/f86/18/21/41/30/valid-10.png',
+      i_icon_attachment_see : 'http://i86.servimg.com/u/f86/18/21/41/30/show-a10.png',
+      i_icon_attachment_download : 'http://i86.servimg.com/u/f86/18/21/41/30/downlo10.png',
+
+      // DO NOT TRANSLATE
+      page : 'buttons',
+      submit : 'Save'
+    },
+
+    gallery : {
+      upload_pic : 'http://i35.servimg.com/u/f35/18/21/60/73/upload11.png?color=primary&size=m'
+    }
+
+  },
+
+
+  javascripts : {
+
+    '[FA EDGE] ALL.JS' : {
+      widget_menu_title : '<div class="title module_column_title">قائمة جانبية</div>',
+      widget_menu_tooltip : "menu.title = 'قائمة جانبية';",
+
+      select_theme : 'الرجاء إختيار لون',
+      random_theme : 'لون عشوائي',
+      default_theme : 'اللون الافتراضي',
+
+      toolbar_search_placeholder : 'بحث...',
+
+      quick_nav_title : "title : 'الإنتقال السريع'",
+      quick_nav_tooltip : "tooltip : 'الإنتقال السريع'",
+      quick_nav_notif : '	الإشعارات',
+      tool_copy_url_tooltip : 'نسخ رابط BBCode',
+      tool_copy_url_prompt : 'إنسخ رابط الـBBCode التالي (CTRL+C)',
+      tool_admin_panel : 'لوحة التحكم',
+      tool_to_top : 'أعلى الصفحة',
+      tool_to_bottom : 'أسفل الصفحة',
+
+      img_resizer_full_size : 'المعاينة بالمقاسات الاصلية',
+      img_resizer_enlarge : 'تكبير الصورة',
+      img_resizer_reduce : 'تصغير الصورة',
+      img_resizer_download : 'تحميل الصورة',
+      img_resizer_tooltip : '	إضغط لمعاينة الصورة كاملة',
+
+      fae_update_found : 'لقد تم إيجاد تحديث لنسختك من فوروماكتيف إيدج Forumactif EDGE... عندما تكون جاهزا، الرجاء التوجه نحو لوحة التحكّم بالنسخة و التحديث بالضغط على "البحث عن تحديثات"'
+    },
+
+
+    '[FA EDGE] TOPICS.JS' : {
+      thanked_message : '<div class="thanked-message"><i class="fa fa-thumbs-up"></i> {USERNAME} قد شكر فاتح الموضوع!</div>'
+    }
+
+  },
+
+
+  templates : {
+    'overall_header.html' : {
+      announcements : '<div id="fa_ticker_title" class="title">الإعلانات</div>'
+    },
+
+
+    'overall_footer_end.html' : {
+      back_to_top : 'العودة إلى الأعلى',
+      fae_feedback : 'إرسال رأيك الشخصي',
+      fae_feedback_tooltip : 'ساهم في تطوير Forumactif EDGE'
+    },
+
+
+    logged_out_reply : {
+      main_title : '<h2 class="lor_maintitle">للمشاركة انت بحاجه الى تسجيل الدخول او التسجيل</h2>',
+      main_desc : '<p class="lor_maindesc">يجب ان تعرف نفسك بتسجيل الدخول او بالاشتراك معنا للمشاركة</p>',
+
+      create_account_title : '<h3 class="lor_subtitle">التسجيل</h3>',
+      create_account_desc : '<p class="lor_subdesc">انضم الينا لن يستغرق منك الا ثوانى معدودة!</p>',
+      create_account_button : '<a href="/register" class="button1">أنشئ حساب جديد</a>',
+
+      log_in_title : '<h3 class="lor_subtitle">تسجيل الدخول</h3>',
+      log_in_desc : '<p class="lor_subdesc">ليس لديك عضويه ؟ بضع ثوانى فقط لتسجيل حساب</p>',
+      log_in_button : '<a href="/login" class="button1">تسجيل الدخول</a>'
+    }
+
+  },
+
+
+  fae_cp : {
+    fae_cp_main_title : 'لوحة التحكم بنسخة Forumactif EDGE',
+    fae_cp_desc : 'هذه اللوحة تسمح لك بثبيت، مسح و تحديث نسختك من <a href="https://github.com/SethClydesdale/forumactif-edge" target="_blank">Forumactif Edge</a>. كن مستعدا لإستكشاف منتداك بحلة جديدة متطورة و سهلة التصفح!',
+    fae_log : 'السجل',
+    fae_actions : 'العمليات',
+
+    fae_install : 'تثبيت',
+    fae_reinstall : 'إعادة التثبيت',
+    fae_install_complete : 'إنتهى التثبيت بنجاح!',
+    fae_install_warning : 'هل أنت متأكد من رغبتك {INSTALL} Forumactif Edge؟ هذا سيؤدّي إلى حذف التصميم الخاص بك و أكواد الجافا سكريبت كذلك!الرجاء التأكد من وضع نسخة إحتياطية من ملفاتك الشخصية (أكواد انسيابية، تومبيلايت و أكواد الجافا سكريبت) قبل المواصلة. الرجاء الضغط على "إلغاء" في حال عدم إستعدادك الكامل لتثبيت Forumactif Edge',
+
+    fae_uninstall : 'حذف النسخة',
+    fae_uninstall_warning : 'هل أنت متأكد من رغبتك حذف نسختك من Forumactif Edge؟ هذا سيؤدي إلى حذف جميع الأكواد الانسيابية، الجافاسكريبت و قوالب التومبلايت و سيتم إرجاع منتداك إلى التصميم الافتراضي لـPHPBB 3الرجاء التأكد من وضع نسخة إحتياطية من ملفاتك الشخصية (أكواد انسيابية، تومبيلايت و أكواد الجافا سكريبت) قبل المواصلة. الرجاء الضغط على "إلغاء" في حال تراجعت عن رأيك في مسح نسختك من Forumactif Edge',
+
+    fae_update : 'البحث عن تحديثات',
+    fae_update_good : 'أنت تمتلك حاليا النسخة الاخيرة من Forumactif Edge!',
+    fae_update_check : 'البحث عن تحديثات في Github...',
+    fae_update_prepare : 'يتم حاليا التحضير للتحديث... الرجاء عدم إقفال النافذة!',
+    fae_update_start : 'سيبدأ التحديث خلال لحظات... الرجاء عدم إقفال النافدة!',
+    fae_update_fetch : 'الحصول على تعليمات التحديث للاصدار {VERSION_STRING}... ({UPDATE_MIN}/{UPDATE_MAX})',
+    fae_update_notes : 'الرجاء <a href="{RELEASE_TAG}" target="_blank">الضغط هنا</a> لمعاينة التطورات التي أجريت للنسخة {VERSION_STRING}.',
+    fae_update_error : 'لم يتم إيجاد تعليمات التحديث للإصدار {VERSION_STRING} . الرجاء <a href="http://fmdesign.forumotion.com/t700-forumactif-edge-support#13923" target="_blank">فتح تذكرة دعم</a> و إخبارنا بهذا الخلل لنتمكن من مساعدتك',
+
+    fae_translate : 'تغيير اللغة',
+    fae_translate_same : 'منتداك يستعمل فعلا اللغة {LANG}. الرجاء إختيار لغة أخرى.',
+    fae_translate_warning : 'هل أنت واثق من رغبتك تحويل لغة منتداك إلى {LANG} ؟',
+    fae_translate_start : 'سيتم ترجمة منتداك خلال لحظات. الرجاء الانتظار...',
+    fae_translate_get : 'الحصول على بيانات اللغة {LANG}',
+    fae_translate_loaded : 'تم تحميل بيانات اللغة. سيتم الترجمة خلال لحظات... الرجاء عدم إقفال هذه النافذة!',
+
+    fae_err_not_founder : 'فقط <a href="/u1">مؤسس المنتدى</a> يمكنه إستعمال هذه اللوحة. ',
+
+    left : 'يسار',
+    center : 'وسط',
+    right : 'يمين',
+    yes : 'نعم',
+    no : 'لا',
+    help : 'أحتاج إلى مساعدة!',
+
+    general_settings : {
+      title : 'إعدادات عامة',
+      fae_gen_desc : 'هذه الفئة تسمح لك بالتعديل على الاعدادات العامة لـForumactif Edge.',
+
+      'fae_label-width' : 'عرض المنتدى :',
+      'fae_gen_tip-width' : 'الرجاء الاستعانة بالمؤشر لضبط عرض منتداك.',
+
+      'fae_label-navbar' : 'موقع أزرار التصفح :',
+      'fae_gen_tip-navbar' : 'موقع أزرار التصفح أعلى المنتدى.',
+
+      'fae_label-logo' : 'موقع اللوغو :',
+      'fae_gen_tip-logo' : 'موقع العارضة الرئيسية للمنتدى',
+
+      'fae_label-profile' : 'البيانات الشخصية :',
+      'fae_gen_tip-profile' : 'موقع البيانات الشخصية للعضو في المساهمات',
+
+      fae_update_general : 'حفظ التغييرات',
+      fae_gen_updating : 'حفظ التغييرات...',
+      fae_gen_updated : 'تم إحداث التغييرات بنجاح!'
+    },
+
+
+    theme_management : {
+      title : 'تصميم المنتدى',
+      fae_theme_desc : 'تسمح لك هذه الفئة بإدارة التصميم الافتراضي للمنتدى و ألوانه و ذلك عبر تحميل التصاميم الجديدة من موقع Github.',
+
+      'fae_label-theme' : 'تصميم المنتدى',
+      'fae_theme_tip-theme' : 'الرجاء إختيار التصميم الذي تريده للمنتدى',
+
+      'fae_label-color' : 'إختيار اللون',
+      'fae_theme_tip-color' : 'الرجاء إختيار اللون الافتراضي للمنتدى.',
+
+      'fae_label-minify' : 'ورقة التصاميم المصغّرة:',
+      'fae_theme_tip-minify' : 'ورقة التصاميم المصغّرة تقلّص من حجم الاكواد الانسيابية، لكنها ليس الاختيار الامثل للتعديل. إذا أردت التعديل على الاكواد الانسيابية، الرجاء اختيار "لا"',
+
+      'fae_label-rtl' : 'الكتابة من اليمين إلى اليسار RTL :',
+      'fae_theme_tip-rtl' : 'تغيير التصميم إلى تصميم متوجّه من اليمين إلى الشمال و ذلك للغات ذات هذا الاتجاه (مثل العربية)',
+
+      fae_import_theme : 'تحميل التصميم',
+      fae_import_confirm : 'هل أنت متأكد من رغبتك تحميل التصميم "{THEME_NAME}" ?الرجاء التأكد من احتفاضك بنسخة احتياطية من ورقة الاكواد الانسيابية في حال رغبتك العودة إليها، لأنه سيتم تغييرها عند تطبيق هذا التصميم. الرجاء الضغط على "إلغاء" في حالة عدم استعدادك التام.'
+    }
+  }
+
+};
