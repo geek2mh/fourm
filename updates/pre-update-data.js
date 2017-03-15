cdd567b134c367b79c84407f6f7626adfbfe038d
geek2mh <geek2mh@gmail.com>
2017-03-15 04:42:09 +0200

    Add files via upload

---
+++ updates/pre-update-data.js
@@ -0,0 +1,16 @@
+// This file is used for testing updates before they go live
+window.forumactif_edge_version_data = [
+  '1.0.0-beta',
+  '1.0.0-beta.1',
+  '1.0.0',
+  '1.0.1',
+  '1.0.2',
+  '1.1.0',
+  '1.1.1',
+  '1.1.2',
+  '1.1.3',
+  '1.1.4',
+  '1.1.5',
+  '1.1.6',
+  '1.1.7'
+];
